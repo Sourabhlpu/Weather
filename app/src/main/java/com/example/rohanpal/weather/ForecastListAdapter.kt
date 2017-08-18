426040f11adfcfa1353fca0f5d2e95ec79e78d78
Sourabh Pal <nick.tdr19@gmail.com>
2017-08-18 17:41:53 +0530

    Use Adapter and display a list of data

---
+++ app/src/main/java/com/example/rohanpal/weather/ForecastListAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.rohanpal.weather
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by rohanpal on 8/18/2017.
+ */
+class ForecastListAdapter(val items: List<String>) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.textView?.text = items[position]
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+
+        return ViewHolder(TextView(parent?.context))
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView : TextView) : RecyclerView.ViewHolder(textView)
+
+}
